e920e68d7c88ed2b05e0c4c033631fcafaa3e10a
jingwei-li <allhappyliferay@gmail.com>
2021-03-19 23:12:05 +0800

    Add empty function for plotting behavioral distributions of matched AA vs WA
    Why:
    
    *
    
    This change addresses the need by:
    
    *

---
+++ plot/HCP_hist_matchedY_AA_WA.m
@@ -0,0 +1,13 @@
+function HCP_hist_matchedY_AA_WA(split_AAdir, split_WAdir, KRR_dir, full_subj_ls, nseeds, outdir)
+
+% HCP_hist_matchedY_AA_WA(split_AAdir, split_WAdir, KRR_dir, full_subj_ls, nseeds, outdir)
+%
+% Long description
+
+addpath(genpath(fullfile(dirname(fileparts(mfilename('fullpath'))), 'external_packages', 'fig_util')))
+
+
+rmpath(genpath(fullfile(dirname(fileparts(mfilename('fullpath'))), 'external_packages', 'fig_util')))
+
+    
+end
